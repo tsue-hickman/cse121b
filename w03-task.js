1585309840cead00f8fdfe4dc1222721363a3849
Tsue_Hic22009 <hic22009@byui.edu>
2024-01-27 09:59:44 -0700

    started the java for w03

---
+++ w03-task.js
@@ -0,0 +1,59 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+function add (number1, number2){
+    return number1 + number2;
+}
+let addNumber1 = Number(document.querySelector("#add1").value);
+
+document.querySelector('#sum').value = add (addNumber1, addNumber2);
+
+document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+
+/* Function Expression - Subtract Numbers */
+function subtract (number1, number2){
+    return number1 - number2;
+}
+let subtractNumber1 = Number(document.querySelector("#subtract1").value);
+
+document.querySelector('#difference').value = subtract (subtractNumber1, subtractNumber2);
+
+document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
+
+
+/* Arrow Function - Multiply Numbers */
+function multiply (number1, number2){
+    return number1 * number2;
+}
+let multiplyNumber1 = Number(document.querySelector("#multiply1").value);
+
+document.querySelector('#product').value = multiply (multiplyNumber1, multiplyNumber2);
+
+document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
+
+/* Open Function Use - Divide Numbers */
+function divide (number1, number2){
+    return number1 / number2;
+}
+let divideNumber1 = Number(document.querySelector("#divide1").value);
+
+document.querySelector('#quotient').value = divide (divideNumber1, divideNumber2);
+
+document.querySelector('#divideNumbers').addEventListener('click', divideNumbers);
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
