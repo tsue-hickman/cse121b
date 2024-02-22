a393a551aa455dd9a77841679fa7ca40514f17aa
Tsue_Hic22009 <hic22009@byui.edu>
2024-02-22 04:40:01 -0700

    i actually connected the js with the html

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -1,7 +1,5 @@
 /* LESSON 3 - Programming Tasks */
 
-/* FUNCTIONS */
-/* Function Definition - Add Numbers */
 function add(number1, number2) {
     return number1 + number2;
 }
@@ -14,7 +12,6 @@ function addNumbers() {
     document.querySelector('#sum').value = add(addNumber1, addNumber2);
 }
 
-/* Function Expression - Subtract Numbers */
 function subtract(number1, number2) {
     return number1 - number2;
 }
@@ -27,7 +24,6 @@ function subtractNumbers() {
     document.querySelector('#difference').value = subtract(subtractNumber1, subtractNumber2);
 }
 
-/* Arrow Function - Multiply Numbers */
 function multiply(number1, number2) {
     return number1 * number2;
 }
@@ -40,7 +36,6 @@ function multiplyNumbers() {
     document.querySelector('#product').value = multiply(multiplyNumber1, multiplyNumber2);
 }
 
-/* Open Function Use - Divide Numbers */
 function divide(number1, number2) {
     return number1 / number2;
 }
@@ -64,28 +59,20 @@ function getTotalDue() {
     document.querySelector("#total").textContent = `Total: $${totalDue.toFixed(2)}`;
 }
 
-/* ARRAY METHODS - Functional Programming */
-/* Output Source Array */
-// Declare and instantiate an array variable to hold the numbers 1 through 13
 let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
-// Assign the value of the array variable to the HTML element with an ID of "array"
+
 document.querySelector('#array').textContent = numbersArray;
 
-// Use the filter() array method to find all odd numbers and assign the result to the HTML element with an ID of "odds"
+
 document.querySelector('#odds').textContent = numbersArray.filter(number => number % 2 === 1);
 
-// Use the filter() array method to find all even numbers and assign the result to the HTML element with an ID of "evens"
 document.querySelector('#evens').textContent = numbersArray.filter(number => number % 2 === 0);
 
-// Use the reduce() array method to sum the array elements and assign the result to the HTML element with an ID of "sumOfArray"
 document.querySelector('#sumOfArray').textContent = numbersArray.reduce((sum, number) => sum + number);
 
-// Use the map() array method to multiply each element by 2 and assign the result to the HTML element with an ID of "multiplied"
 document.querySelector('#multiplied').textContent = numbersArray.map(number => number * 2);
 
-// Use the map() and reduce() array methods to sum the array elements after multiplying each element by two.
-// Assign the result to the HTML element with an ID of "sumOfMultiplied"
 document.querySelector('#sumOfMultiplied').textContent = numbersArray
     .map(number => number * 2)
     .reduce((sum, number) => sum + number);
